f69b26d8a79022d356d9c578c91dfc04757c5360
mldamico <matias.lioneldamico@gmail.com>
2023-09-19 18:35:10 -0300

    Get brands and types repository and controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -29,4 +29,17 @@ public class ProductsController : ControllerBase
     {
         return Ok(_productRepository.GetProductByIdAsync(id));
     }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+    {
+        return Ok(await _productRepository.GetProductBrandsAsync());
+    }
+    
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+        return Ok(await _productRepository.GetProductTypesAsync());
+    }
+    
 }
