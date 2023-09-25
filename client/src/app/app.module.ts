0de8144f150d32506e9e4831a74e1873471502de
mldamico <matias.lioneldamico@gmail.com>
2023-09-25 18:43:04 -0300

    Pagination

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    ShopModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
