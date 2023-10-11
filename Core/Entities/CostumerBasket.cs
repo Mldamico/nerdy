bca7dc92df4198b249b9795ff7c9665d78acc38b
mldamico <matias.lioneldamico@gmail.com>
2023-10-11 09:14:23 -0300

    BasketItem and basket entities

---
+++ Core/Entities/CostumerBasket.cs
@@ -0,0 +1,16 @@
+namespace Core.Entities;
+
+public class CostumerBasket
+{
+    public CostumerBasket()
+    {
+        
+    }
+    public CostumerBasket(string id)
+    {
+        Id = id;
+    }
+
+    public string Id { get; set; }
+    public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+}
