a124d80898898b25f028d01cef4315ef41957ee6
mldamico <matias.lioneldamico@gmail.com>
2023-11-26 15:46:52 -0300

    Shared basket summary

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -5,17 +5,16 @@ import { BasketItem } from '../shared/models/basket';
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.scss']
+  styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent {
-
-  constructor(public basketService: BasketService) { }
+  constructor(public basketService: BasketService) {}
 
   incrementQuantity(item: BasketItem) {
     this.basketService.addItemToBasket(item);
   }
 
-  removeItem(id: number, quantity: number) {
-    this.basketService.removeItemFromBasket(id, quantity);
+  removeItem(event: { id: number; quantity: number }) {
+    this.basketService.removeItemFromBasket(event.id, event.quantity);
   }
 }
